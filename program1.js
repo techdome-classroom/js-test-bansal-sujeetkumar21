7d0110dc6f0b56c28b99047a1ccd7143fc8c50c2
SUJEET KUMAR <108637141+sujeetkumar21@users.noreply.github.com>
2024-02-23 07:49:33 +0000

    done

--- program1.js
+++ program1.js
@@ -3,9 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+
+    const stack = []
+    const open = {
+        "(": true,
+        "{": true,
+        "[": true
+    }
+    const corresponding = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (open[s[i]]) {
+            stack.push(s[i])
+        } else if (stack.length == 0) {
+            return false
+        } else if (corresponding[s[i]] == stack[stack.length - 1]) {
+            stack.pop()
+        } else {
+            return false
+        }
+    }
+    return stack.length == 0
     
 };
 
 module.exports = { isValid };
-
 
--- program2.js
+++ program2.js
@@ -4,6 +4,29 @@
  */
 var romanToInt = function(s) {
     
+    let roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+      };
+      
+      let letters = s.split("");
+      let res = 0;
+      //IX
+      for (var i = 0; i < letters.length; i++) {
+          if (roman[ letters[i] ] < roman[ letters[ i + 1 ] ] ) {
+              res -= roman[letters[i]]
+          } else {
+              res += roman[letters[i]]
+          }
+      }
+      return res;
+
+ 
 };
 
 
